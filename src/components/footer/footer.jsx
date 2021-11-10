50e6e0826f9cee9918880ba132564822aea17081
Wei-Tong Tang <weitongblue@gmail.com>
2021-11-10 17:58:32 +0000

    added footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Drinking Games by Wei-Tong Tang and Salim Abdala a fucking smelly cunt
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import Footer from '../../components/footer/footer'
 import Nav from '../../components/nav/nav'
 import './home.scss'
 
-const Home = (props) => {
+const Home = () => {
 
     return (
         <div className="home">
             <Nav title="Home" goBack={false}/>
             <div className="home__content">
-                
+                <div className="home__content-higher"></div>
+                <div className="home__content-shithead"></div>
+                <div className="home__content-blackjack"></div>
+                <div className="home__content-bus"></div>
             </div>
+            <Footer/>
         </div>
     )
 }
