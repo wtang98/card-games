ddd9e3869bc635dee62797c8e2504af84bf851e3
Angaar Uriakhil <angar1996@gmail.com>
2021-11-19 19:23:43 +0000

    fixed errors with higher page by implementing state. cleaned up code and fixed logic for dealing a random card and removing it from the deck. set up useEffect for logging the changes to the console.

--- src/pages/higher/higher.jsx
+++ src/pages/higher/higher.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 import ReactDOM, { render } from 'react-dom';
 import './higher.scss'
 import Nav from '../../components/nav/nav'
@@ -6,41 +6,57 @@ import { Button } from '@material-ui/core'
 
 const Higher = (props) => {
 
-    const {deckOfCards} = props
+    const {deckOfCards} = props; 
     const [randomValue, setRandomValue] = useState(); 
-    // const 
-    let copy = [...deckOfCards()];
-    
-    
-    let random = 52;
-    const dealtCard = Math.floor(Math.random()*random);
-    const getRandomCard = () => {
-        if(random>=0){
-            random-=1;
-            console.log(random);
-            return copy[dealtCard];
+    const [dealtCardIndex, setDealtCardIndex] = useState(0); 
+    const [dealtCardIndexArr, setDealtCardIndexArr] = useState([]);
+    const [deck, setDeck] = useState(deckOfCards()); 
+
+    const setupIndexArr = () => {
+        // make an array of indices with 52 numbers, 0-52 and set it to a state. 
+        let dealtCardIndexArrTemp = [];
+        for (let i=0; i<52; i++) {
+            dealtCardIndexArrTemp.push(i); 
         }
+        setDealtCardIndexArr(dealtCardIndexArrTemp); 
     }
-    const removeCard = () => {
-        copy[dealtCard] = copy[copy.length-1];
-        copy.pop();
-        console.log(copy);
-        return copy;
-    }
-    const callBoth = () => {
-        const randomCard = getRandomCard();
-        console.log(randomCard.Value);
-        setRandomValue(randomCard.Value); 
-        removeCard();
+    // state changes are asynchronous, meaning to log the updated values you must log in a function defined in a useEffect, which will log to the console whenever the value of deck state or dealtCardIndexArr changes. 
+    const logValues = () => {
+        console.log(deck); 
+        console.log(dealtCardIndexArr); 
     }
 
-    // callBoth();
+    useEffect(setupIndexArr, []); 
+    useEffect(logValues, [deck, dealtCardIndexArr]); 
+
+    const getRandomCard = () => {
+        // generate random index from array of indices. 
+        setDealtCardIndex(Math.floor(Math.random()*dealtCardIndexArr.length));
+        // return the card. 
+        setRandomValue(deck[dealtCardIndex].Value)
+    }
+    const removeCard = () => {
+        // Make sure there are cards left to remove in the deck
+        if (dealtCardIndexArr.length > 0) {
+            console.log(dealtCardIndex);
+            // remove the card from the deck array and also remove the index from the indexes array.  
+            setDeck(deck.filter(item => item !== deck[dealtCardIndex])); 
+            setDealtCardIndexArr(dealtCardIndexArr.filter(item => item !== dealtCardIndexArr[dealtCardIndex] ))
+        } else {
+            console.log("All cards have been removed already.")
+        }
+        
+    }
+    const getCardAndRemoveIt = () => {
+        getRandomCard(); 
+        removeCard();
+    }
 
     return (
         <div className="higher">
             <Nav title="Higher or Lower" goBack={true}/>
             <div className="higher__content">
-                <Button variant="contained" onClick={removeCard}>fat mf</Button>
+                <Button variant="contained" onClick={getCardAndRemoveIt}>Action</Button>
                 <div id="higherValue">
                     <p>{randomValue} </p> 
                 </div>
