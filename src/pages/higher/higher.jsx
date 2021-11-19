b856077d91331759aa23a27d048d5b50ef90ad34
Wei-Tong Tang <weitongblue@gmail.com>
2021-11-19 11:08:32 +0000

    problems with getting random card and removing said card

--- src/pages/higher/higher.jsx
+++ src/pages/higher/higher.jsx
@@ -6,40 +6,43 @@ import { Button } from '@material-ui/core'
 
 const Higher = (props) => {
 
-    const [randomValue, setRandomValue] = useState(); 
     const {deckOfCards} = props
+    const [randomValue, setRandomValue] = useState(); 
+    // const 
     let copy = [...deckOfCards()];
     
     
     let random = 52;
     const dealtCard = Math.floor(Math.random()*random);
     const getRandomCard = () => {
-        random--;
-        return copy[dealtCard];
+        if(random>=0){
+            random-=1;
+            console.log(random);
+            return copy[dealtCard];
+        }
     }
-    // console.log(getRandomCard().Value);
-    
     const removeCard = () => {
         copy[dealtCard] = copy[copy.length-1];
         copy.pop();
+        console.log(copy);
         return copy;
     }
-    
     const callBoth = () => {
         const randomCard = getRandomCard();
         console.log(randomCard.Value);
-        // ReactDOM.render(<p>{getRandomCard().Value}</p>, document.getElementById("higherValue"));
         setRandomValue(randomCard.Value); 
         removeCard();
     }
+
     // callBoth();
+
     return (
         <div className="higher">
             <Nav title="Higher or Lower" goBack={true}/>
             <div className="higher__content">
-                <Button variant="contained" onClick={callBoth}>fat mf</Button>
+                <Button variant="contained" onClick={removeCard}>fat mf</Button>
                 <div id="higherValue">
-                   <p> {randomValue} </p> 
+                    <p>{randomValue} </p> 
                 </div>
             </div>
         </div>
